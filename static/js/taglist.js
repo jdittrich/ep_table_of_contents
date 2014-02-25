bece31b9e086ec44c96407294814a5659f4a8597
JAN DITTRICH <d_jan@ymail.com>
2014-02-25 22:07:26 +0100

    hooks

--- static/js/taglist.js
+++ static/js/taglist.js
@@ -16,11 +16,21 @@ var taglist = {
 		$('#editorcontainer').css("width", "100%");
 	},
 
+	
+	
+	/* depreciated was code form the TOC-plugin
 	// Find Tags
+	
 	findTags: function(){
 		var toc = {};
 		var count = 0;
-		var hs = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").children("div").children("h1, h2, h3, h4, h5, h6");
+		var hs = $('iframe[name="ace_outer"]').
+			contents().
+			find('iframe').
+			contents().
+			find("#innerdocbody").
+			children("div").
+			children("h1, h2, h3, h4, h5, h6");
 		$(hs).each(function(){ //for each relevant dom element
 			var tag = this.nodeName.toLowerCase();
 			var newY = $(this).context.offsetTop + "px";
@@ -34,13 +44,15 @@ var taglist = {
 			}
 		count++;
 	}); //each end
-
+	*/
+	
+	/* depreciated was code form the TOC-plugin
 	$.each(toc, function(h, v){ // for each item we want to display
 		var TOCString = "<a title='"+v.text+"' class='taglistItem taglist"+v.tag+"' data-class='taglist"+v.tag+"' onClick=\"taglist.scroll('"+v.y+"');\" data-offset='"+v.y+"'>"+v.text+"</a>";
 		$('#taglistItems').append(TOCString); //display it. TODO: Doing this iteratively, is probably inefficient.
 	});
 
-	},
+	},*/
 
 
 	// get HTML
@@ -56,6 +68,12 @@ var taglist = {
 	},
 
 	createTaglist:function(){
+		/*
+		gets: nothing
+		returns: a key-value(Strings)structure like {a:[arrays], b:[arrays]… etc.}. Currenty depth of 1, later hopefully more <- TODO
+		does: get text from document search for a regex and gather all text+sourrounding dom element in the return value. 
+		*/
+		
 		var hits=[]; //hits save as [{domElement,
 
 		$('iframe[name="ace_outer"]')
@@ -65,7 +83,9 @@ var taglist = {
 		.find("#innerdocbody")
 		.children("div")
 		.each(function(index,domElement){ //get all dom nodes of interest. For each do...
-			var stringMatches =  $(domElement).text().match(/(#\w+)+/g) || []; //finds all (/g = greedy regex) matches in the inner text of the current DOM element //TODO: move regex to config object
+	
+			//finds all (/g = greedy regex) matches in the inner text of the current DOM element //TODO: move regex to config object
+			var stringMatches =  $(domElement).text().match(/(#\w+)+/g) || []; 
 			underscore.each(stringMatches,function(stringMatch, index, list){
 			hits.push({
 				relatedDomElement:domElement,
@@ -75,16 +95,23 @@ var taglist = {
 		});
 
 		//now that we got all our hits in an [{domEleme,[hash1, hash2]}, ] structure…
-		var sortedList = underscore.sortBy(hits,function(objects){return objects.match.join("");});//sort all entries in the hits array alphabetically
+		var sortedList = underscore.sortBy(hits,function(objects){return objects.match.join("");});//sort all entries in the hits array alphabetically. The sub-tag-Array-Items are joined for that.
 		var groupedTree = underscore.groupBy(sortedList,function(objects){
 			return objects.match[0];
 			}); //input: testarray=[["a","s"],["a","p"],["a","d"],["b","s"],["b","p"],["c","d"]], outputs {a:[/*all arrays with first element being an a*/], b:[/*all arrays with first element being an b*/]… etc.}
 		return groupedTree;
 	},
+	
+	
 	renderTaglist:function(groupedTree){
+		/*
+		gets: a key-value(Strings)structure like {a:[arrays], b:[arrays]… etc.}. Currenty depth of 1, later hopefully more <- TODO
+		returns: undefined
+		does: create DOM-Elements for displaying a scroll-on-click overview of all tags in the document
+		*/
 		var taggroupsArray=[];
-		$.each(groupedTree, function(key,matches){
-			var groupHeading = $("<h2/>",{'class':"ep_taglist_Heading",'text':key}); //inner content?
+		$.each(groupedTree, function(key,matches){ 
+			var groupHeading = $("<h2/>",{'class':"ep_taglist_Heading",'text':key}); 
 			var groupMatchesContainer = $("<ul/>",{'class':"ep_taglist_groupMatchesContainer"});
 			$.each(matches,function(index,value){
 				var listItem=$("<li/>",{class:"ep_taglist_groupEntry", text:value.match.join("")}).on("click",function(){
@@ -123,6 +150,10 @@ var taglist = {
 	if ( [temp[0]] == sname ) { sval = temp[1]; }
 	}
 	return sval;
+	},
+	
+	keyEvent: function(arg1, arg2, arg3, arg4, arg5){
+		"thisisastring"
 	}
 };
 
