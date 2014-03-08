a18429438261f1d6c7f8e5b261db9d7bb1b5d283
JAN DITTRICH <d_jan@ymail.com>
2014-03-08 13:58:01 +0100

    height fix for sidebar

--- static/js/postAceInit.js
+++ static/js/postAceInit.js
@@ -1,7 +1,7 @@
 exports.postAceInit = function(){
+var $ = require('ep_etherpad-lite/static/js/rjquery').$; //it rjquery is a bridge in order to make jquery require-able
+$("#taglist").css("height","calc(100% - "+$("#editbar").height()+"px"+")"); //the fixed position of the taglist-sidebar is calculated for the viewport height. The editbar reduces the effective height that can be used, thus we need to reduce the height by that amount. Why the computed value of "top" for the element is the viewports height already (and not 0) I dont know.
 
-
-  /* on click */
   
   //  "#options-taglist" is simply the id/selector of the input with the checkbox determining if taglist is toggled or not. 
   $('#options-taglist').on('click', function() {
